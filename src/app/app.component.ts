6be06aa8e3ad1736df6ffe0f628987a4bd3d5142
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2023-12-09 22:25:31 -0700

    fixed imports in app component after deleting new-task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TasksComponent } from './components/tasks/tasks.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { NewTaskComponent } from "./components/new-task/new-task.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,13 +8,7 @@ import { RouterOutlet } from '@angular/router';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [
-      CommonModule, 
-      RouterOutlet,
-      NavbarComponent, 
-      TasksComponent, 
-      HomeComponent, 
-      NewTaskComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
   logo: string = 'https://www.ldatschool.ca/wp-content/uploads/2017/04/Pen-making-check-marks-in-boxes.jpg';
